5acf24913724ac8cb06c521084f6b617e9932295
Akanksha-H <aakanksha0499@gmail.com>
2023-02-06 22:03:27 +0530

    Added: New Contact Information

--- src/com/bridgelabz/addressbook/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/addressbook/AddressBookMain.java
@@ -3,5 +3,7 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO ADDRESS BOOK SYSTEM");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
     }
 }
