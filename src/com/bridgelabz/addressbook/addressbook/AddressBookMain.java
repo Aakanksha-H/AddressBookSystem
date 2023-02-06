db6581379f0af7cb8347e3be366e54932960c710
Akanksha-H <aakanksha0499@gmail.com>
2023-02-06 21:59:18 +0530

    Created: New Contact Information

--- src/com/bridgelabz/addressbook/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/addressbook/AddressBookMain.java
@@ -1,6 +1,6 @@
-package com.bridgelabz.addressbook;
-
+package com.bridgelabz.addressbook.addressbook;
 public class AddressBookMain {
+
     public static void main(String[] args) {
         System.out.println("WELCOME TO ADDRESS BOOK SYSTEM");
     }
